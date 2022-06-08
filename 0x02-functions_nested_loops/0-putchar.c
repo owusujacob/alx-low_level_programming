61471eb1f57cf6afe93f85ff86713c6da135ff4a
Owusu Jacob <55759428+owusujacob@users.noreply.github.com>
2022-06-08 01:49:59 +0000

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,25 +1,17 @@
-#include <unistd.h>
-
-
-
+#include "main.h"
 /**
+ * print_alphabet - Prints the alphabet in lowercase
  *
- *  * _putchar - writes the character c to stdout
- *
- *   * @c: The character to print
- *
- *    *
- *
- *     * Return: On success 1.
- *
- *      * On error, -1 is returned, and errno is set appropriately.
- *
- *       */
+ * Return: Always 0 (Success)
+ */
 
-int _putchar(char c)
-
+void print_alphabet(void)
 {
+	char alpha;
 
-		return (write(1, &c, 1));
-
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		_putchar (alpha);
+	}
+	_putchar('\n');
 }
