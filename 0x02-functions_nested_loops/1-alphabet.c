f130614dfc3c091a0e52ee9a9e2cc8c996b17e46
Owusu Jacob <55759428+owusujacob@users.noreply.github.com>
2022-06-08 01:51:01 +0000

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,33 +1,19 @@
 #include "main.h"
-
 /**
+ * main - Entry point
  *
- *  * print_alphabet - Prints the alphabet in lowercase
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
-
-
-
-void print_alphabet(void)
-
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-
-		char alpha;
-
-
-
-			for (alpha = 'a'; alpha <= 'z'; alpha++)
-
-					{
-
-								_putchar (alpha);
-
-									}
-
-				_putchar('\n');
-
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
